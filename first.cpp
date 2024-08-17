f0df8badd93264507795a0d68d33912e6ffeb5ce
Muskann27 <165546240+Muskann27@users.noreply.github.com>
2024-08-17 20:35:28 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main (){
+    cout<<"hello";
+    int x;
+    cout<<"\n hi : ";
+    cin>>x;
+    cout<<"done";
+    cout<<"\n";
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<"dumb " " ";
+    }
+    
+    return 0;
+}
